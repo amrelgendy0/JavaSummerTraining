a6a3e1192b15177c9f1fc109706bed4b89a20b17
Amr Elgendy <elgendy0@hotmail.com>
2021-08-18 04:12:34 +0200

    add static drugs data if no sql server found

--- src/com/company/DataManager.java
+++ src/com/company/DataManager.java
@@ -1,8 +1,7 @@
 package com.company;
 import com.company.model.*;
 import java.sql.*;
-import java.util.ArrayList;
-import java.util.HashSet;
+import java.util.*;
 
 public class DataManager {
  private static   HashSet<User> users = new HashSet<User>();
@@ -107,6 +106,7 @@ public static ArrayList<Drug> searchMedicine(String key)  {
 
         } catch (SQLException throwables) {
             System.out.println("please run mySql first");
+
         }
 
         return patients;
@@ -160,6 +160,8 @@ if(con==null){
 
         } catch (SQLException throwables) {
             System.out.println("please run mySql first");
+
+            drugs.addAll(Arrays.asList(getDrugsLocalIfSqlServerNotFound()));
         }
         return drugs;
     }
@@ -172,5 +174,40 @@ if(con==null){
     public static HashSet<User> users() {
         return users;
     }
+
+   private static Drug[] getDrugsLocalIfSqlServerNotFound(){
+
+
+        Drug[] drug = new Drug[]{
+                new Drug("PANADOL COLD & FLU DAY 24 TAB",27,"30/09/2022",15524501,"Medications"),
+                new Drug("ALCOHOL 70% 100 ML",18,"20/06/2025",15524502,"Sanitizers / Hand Gel"),
+                new Drug("C-RETARD ( VITAMIN C ) 500 MG 10 CAP",16,"01/01/2024",15524503,"VITAMIN / Immunity Enhancer"),
+                new Drug("OMEGA – 3 – PLUS 30 CAP",41.75,"20/06/2025",15524504,"Medicines for regulating blood fats / Immunity Enhancer"),
+                new Drug("GARNIER BY NUTRISSE H.COLOR CR. 7.11",55,"12/12/2023",15524505,"Hair Care"),
+                new Drug("ORAL-B 40 ULTRA THIN BLACK SOFT (2×1)",53,"30/09/2022",15524506,"Toothbrush"),
+                new Drug("ASPIRIN PROTECT 100 MG 20 TAB",21,"20/06/2025",15524507,"Medications / blood"),
+                new Drug("VASELINE PURE PETROLEUM JELLY 50ML ORGINAL",21,"12/12/2023",15524508,"Body Moisturizers"),
+                new Drug("CALCIUM CARBONATE",59,"30/09/2022",15524509,"VITAMIN / Immunity Enhancer"),
+                new Drug("ASPOCID 75 MG 20 TAB",6,"20/06/2025",15524510,"Medications"),
+                new Drug("HEAD&SHOULDERS DANDRUFF SH. 400ML MENTHOL",48,"01/01/2024",15524511,"Hair Care"),
+                new Drug("PLETAAL 50 MG 20 TAB",70,"11/03/2023",15524512,"Medications"),
+                new Drug("SENSODYNE TOOTHPASTE 50GM MULTI CARE & WHITENING",33,"20/06/2025",15524513,"Toothpaste"),
+                new Drug("STROKA 75 MG 30 TAB",145,"30/09/2022",15524514,"Medications"),
+                new Drug("CORASORE 150 MG 20 TAB",16,"01/01/2024",15524515,"Medications"),
+                new Drug("LUX SHW.GEL250ML MAGICAL BEAUTY",33,"11/03/2023",15524516,"Body Care"),
+                new Drug("VIDROP (VITAMIN – D3) ORAL DROPS 15 ML",10.5,"30/09/2022",15524517,"VITAMIN / Immunity Enhancer"),
+                new Drug("COMTREX ACUTE HEAD COLD 20 TAB",30,"11/03/2023",15524518,"Medications / RESPIRATORY DRUGS"),
+                new Drug("LUNA SUN TAN OIL SPR. 130ML SPF15",92,"20/06/2025",15524519,"Sun Care"),
+                new Drug("ORLY 120 MG 30 CAP",97.5,"01/01/2024",15524520,"WEIGHT LOSS AND WEIGHT CONTROL DRUGS"),
+                new Drug("DIET SWEET 100 TAB",13,"11/03/2023",15524521,"WEIGHT LOSS AND WEIGHT CONTROL DRUGS"),
+                new Drug("ANTIFLU 20 CAP",12.75,"30/09/2022",15524522,"Medications / RESPIRATORY DRUG"),
+                new Drug("FA DEOD SPR. 150ML WHITENING CARE",48.25,"11/03/2023",15524523,"Body Care"),
+                new Drug("NIGHT CALM 3 MG 20 TAB",33,"01/01/2024",15524524,"MEDICINES AFFECTING THE NERVOUS SYSTEM"),
+                new Drug("BIVATRACIN 150 ML POWDER SPRAY",36,"20/06/2025",15524525,"TOPICAL PREPARATIONS")
+        };
+
+        return drug;
+    }
+
 }
 
