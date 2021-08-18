cf0594d4e68b5894b909b79f0ba69e76382a82e4
ModyAyman18 <modyhemead@gmail.com>
2021-08-18 03:39:16 +0200

    real prices

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,34 @@ import com.company.FirstChoice;
 public class Main {
     public static void main(String args[]) {
         new FirstChoice();
+        new Drug("ALCOHOL 70% 100 ML",18,"20/06/2025",15524501,"Sanitizers / Hand Gel");
+        new Drug("VIDROP (VITAMIN – D3) ORAL DROPS 15 ML",10.5,"30/09/2022",15524502,"VITAMIN / Immunity Enhancer");
+        new Drug("C-RETARD ( VITAMIN C ) 500 MG 10 CAP",16,"01/01/2024",15524503,"VITAMIN / Immunity Enhancer");
+        new Drug("OMEGA – 3 – PLUS 30 CAP",41.75,"20/06/2025",15524504,"Medicines for regulating blood fats / Immunity Enhancer");
+        new Drug("GARNIER BY NUTRISSE H.COLOR CR. 7.11",55,"12/12/2023",15524505,"Hair Care");
+        new Drug("ORAL-B 40 ULTRA THIN BLACK SOFT (2×1)",53,"30/09/2022",15524506,"Toothbrush");
+        new Drug("ASPIRIN PROTECT 100 MG 20 TAB",21,"20/06/2025",15524507,"Medications / blood");
+        new Drug("VASELINE PURE PETROLEUM JELLY 50ML ORGINAL",21,"12/12/2023",15524508,"Body Moisturizers");
+        new Drug("",0,"30/09/2022",15524509,"");
+        new Drug("",0,"20/06/2025",15524510,"");
+        new Drug("HEAD&SHOULDERS DANDRUFF SH. 400ML MENTHOL",48,"01/01/2024",15524511,"Hair Care");
+        new Drug("",0,"11/03/2023",15524512,"");
+        new Drug("SENSODYNE TOOTHPASTE 50GM MULTI CARE & WHITENING",33,"20/06/2025",15524513,"Toothpaste");
+        new Drug("",0,"30/09/2022",15524514,"");
+        new Drug("",0,"01/01/2024",15524515,"");
+        new Drug("LUX SHW.GEL250ML MAGICAL BEAUTY",33,"11/03/2023",15524516,"Body Care");
+        new Drug("",0,"30/09/2022",15524517,"");
+        new Drug("",0,"11/03/2023",15524518,"");
+        new Drug("LUNA SUN TAN OIL SPR. 130ML SPF15",92,"20/06/2025",15524519,"Sun Care");
+        new Drug("",0,"01/01/2024",15524520,"");
+        new Drug("",0,"11/03/2023",15524521,"");
+        new Drug("",0,"30/09/2022",15524522,"");
+        new Drug("FA DEOD SPR. 150ML WHITENING CARE",48.25,"11/03/2023",15524523,"Body Care");
+        new Drug("",0,"01/01/2024",15524524,"");
+        new Drug("",0,"20/06/2025",15524525,"");
     }
+}
+
+class Drug{
+    Drug(String name, double price, String ExpireDate, int id, String Category){}
 }
